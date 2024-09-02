da8fd09f6729a26c5d28a92e97cb3dc115cef67b
Philiphe Siqueira Ferreira <49068956+philipheferreira@users.noreply.github.com>
2024-09-02 09:29:38 -0300

    primeiro commit

---
+++ reverberacao/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+
+
+namespace reverberacao{
+    class Program{
+
+        static void Main(string[] args){
+
+            int menu;
+
+            do
+            {
+                Console.WriteLine("Menu de operações:\n1 - Materias Cadastrados.\n2 - Adicionar material.\n3 - Deletar material.\n0 - Sair");
+                menu = Convert.ToInt32(Console.ReadLine());
+                switch (menu){
+                    case 0:
+                        Console.WriteLine("Saindo do programa, obrigado por utilizar");
+                        break;
+                    case 1:
+                        Console.WriteLine("Materias cadastrados");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("adicionar material");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Deletar material");
+                        break;
+
+                    default:
+                        Console.WriteLine("Operação não registrada");
+                        break;
+
+                }
+
+            } while ( menu != 0);
+
+        }
+    }
+}
